1c18afbfa83731f63c27e9a4142eb8cc1d3c0f70
juniorh28 <juniorh2897@gmail.com>
2021-01-11 11:24:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 //ContactCard is the name of the function in contactCard.jsx
 //we're importing that function
-import ContactCard from "./contactCard";
+import ContactCard from "./ContactCard";
 import "./App.css";
 //import React and use destructuring to get Component
 import React, { Component } from "react";
 
 //a react class must extend component, you must import React and use destructuring
 class App extends Component {
-  //the class must have a constructor and a super, which may or may not have props
-  constructor() {
-    super();
-    this.state = { userOne: { name: "Tom", phone: "555-555-5555" } };
-  }
+  //We make a state that will have an object called user which it can interate through
+  state = {
+    users: [
+      {
+        name: "Tomm",
+        phone: "555-555-5555",
+        email: "Tom@email.com",
+      },
+      {
+        name: "Jimm",
+        phone: "999-999-9999",
+        email: "Tim@email.com",
+      },
+    ],
+  };
+
+  //this.state.users.map is taking the user object and maping over it, going through each element
+  //we assign the properties inside their elements to the props we create, i.e name, phone, email
+  //these names will then be taken to ContactCard and print out its messages and the props it was given.
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1> Hello You! </h1>
-          <ContactCard user={this.state.userOne} />
+          <h1> Hello! </h1>
+
+          {this.state.users.map((item) => (
+            <ContactCard
+              name={item.name}
+              phone={item.phone}
+              email={item.email}
+            />
+          ))}
         </header>{" "}
       </div>
     );
--- src/contactCard.jsx
+++ src/contactCard.jsx
@@ -1,15 +1,40 @@
-import { render } from "react-dom";
+import React, { Component } from "react";
 
-//we're exporting this function so we use export 
+//we're exporting this function so we use export
 //we want the file importing this function to have the same name as this function
 //in that case we use default so we can use the name of the function as our import
-export default function ContactCard(){
+class ContactCard extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
-      <div>
-        <h1 className="card-name">{props.name="Tom"}!</h1>
-        <h2 className="card-phone">{props.phone="555-555-5555"}</h2>
+      <div className="personComponent">
+        <p>Name: {this.props.name}</p>
+        <p>Email: {this.props.email}</p>
+        <p>Phone: {this.props.phone}</p>
       </div>
     );
   }
 }
+export default ContactCard;
+/*
+ContactCard.propTypes = {
+  name: PropTypes.string,
+  phone: PropTypes.string,
+  email: PropTypes.string,
+};
+*/
+
+/*inside render()
+        <h1 className="card-name">{props.name}!</h1>
+        <h2 className="card-phone">{props.phone}</h2>
+        <h2 className="card-email">{props.email}</h2>
+*/
+/*
+ContactCard.propTypes = {
+  name: "Tom",
+  phone: "555-555-5555",
+  email: "Tom@email.com",
+};
+*/
